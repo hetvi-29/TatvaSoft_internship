45a926801d9a666fb8610be323537013a323257d
HETVI PATEL <123258523+hetvi-29@users.noreply.github.com>
2023-06-05 22:03:28 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Searchbar from "./Components/Searchbar";
+import { AuthWarpper } from "./context/auth";
+import MyNavigation from "./MyNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthWarpper>
+        <ToastContainer />
+        <Header />
+        <Searchbar />
+        <MyNavigation />
+        <Footer />
+      </AuthWarpper>
+    </BrowserRouter>
   );
 }
 
